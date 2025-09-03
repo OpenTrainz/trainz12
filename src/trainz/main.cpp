9f88563d26c0741bd6a7a8c0d36d91ec52c350f5
Yehor Avramenko <yehor.avramenko@ukr.net>
2025-09-03 17:38:36 +0300

    CMake project setup

--- src/trainz/main.cpp
+++ src/trainz/main.cpp
@@ -1,7 +1,7 @@
 #include <Windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine, int nShowCmd)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+                  _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
   MessageBox(NULL, "Hey!", "Trainz12", MB_ICONINFORMATION | MB_OK);
 
